55289ac535c076e2e88752de725328f6bff16212
Sconix <111668367+SCONIX-official@users.noreply.github.com>
2023-11-20 06:44:54 +0500

    END BLIN))

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,17 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function() {
+  let currentScrollPos = window.scrollY;
+  
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("site-header").classList.remove("hidden");
+  } else {
+    document.getElementById("site-header").classList.add("hidden");
+  }
+
+  prevScrollPos = currentScrollPos;
+};
